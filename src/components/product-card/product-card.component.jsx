e3c6d2915187621b3bd6974f301da55bbabed724
Leonardo Del Toro <46249112+LeonardoSDT@users.noreply.github.com>
2023-11-07 19:12:07 +0000

    Update 4 files

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -8,7 +8,7 @@ import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  const {addItemToCart} = useContext(CartContext);
+  const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
 
@@ -17,9 +17,11 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
-      <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
